indraazimi/galeri-hewan

Menambahkan view binding

app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
@@ -12,13 +12,17 @@ package com.indraazimi.galerihewan
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.galerihewan.databinding.ActivityMainBinding
 import com.indraazimi.galerihewan.model.Hewan
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.d("MainActivity", "Jumlah data: " + getData().size)
     }

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/MainActivity.kt
@@ -12,12 +12,16 @@ package com.indraazimi.mobpro1
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.mobpro1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.d("MainActivity", "Jumlah data: " + getData().size)
     }