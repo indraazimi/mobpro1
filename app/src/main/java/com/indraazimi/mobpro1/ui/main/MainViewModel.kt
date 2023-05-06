indraazimi/galeri-hewan

Mengganti arsitektur menjadi MVVM

app/src/main/java/com/indraazimi/galerihewan/ui/main/MainViewModel.kt
@@ -1,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.galerihewan.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.indraazimi.galerihewan.R
+import com.indraazimi.galerihewan.model.Hewan
+
+class MainViewModel : ViewModel() {
+
+    private val data = MutableLiveData<List<Hewan>>()
+
+    init {
+        data.value = initData()
+    }
+
+    // Data ini akan kita ambil dari server di langkah selanjutnya
+    private fun initData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
+        )
+    }
+
+    fun getData(): LiveData<List<Hewan>> = data
+}

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/ui/main/MainViewModel.kt
@@ -1,0 +1,43 @@
+/*
+ * Copyright (c) 2021-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.indraazimi.mobpro1.R
+import com.indraazimi.mobpro1.model.Hewan
+
+class MainViewModel : ViewModel() {
+
+    private val data = MutableLiveData<List<Hewan>>()
+
+    init {
+        data.value = initData()
+    }
+
+    // Data ini akan kita ambil dari server di langkah selanjutnya
+    private fun initData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
+        )
+    }
+
+    fun getData(): LiveData<List<Hewan>> = data
+}