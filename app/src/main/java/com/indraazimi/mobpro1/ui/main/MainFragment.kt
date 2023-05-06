indraazimi/galeri-hewan

Refactor fitur main ke fragment

app/src/main/java/com/indraazimi/galerihewan/ui/main/MainFragment.kt
@@ -1,0 +1,54 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.galerihewan.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.galerihewan.R
+import com.indraazimi.galerihewan.databinding.FragmentMainBinding
+import com.indraazimi.galerihewan.model.Hewan
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter = MainAdapter(getData())
+            setHasFixedSize(true)
+        }
+        return binding.root
+    }
+
+    // Biasanya kita mengambil data dari database, atau server.
+    // Tapi karena materi belum sampai, kita buat dummy saja.
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
+        )
+    }
+}

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/ui/main/MainFragment.kt
@@ -1,0 +1,54 @@
+/*
+ * Copyright (c) 2021-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.mobpro1.R
+import com.indraazimi.mobpro1.databinding.FragmentMainBinding
+import com.indraazimi.mobpro1.model.Hewan
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter = MainAdapter(getData())
+            setHasFixedSize(true)
+        }
+        return binding.root
+    }
+
+    // Biasanya kita mengambil data dari database, atau server.
+    // Tapi karena materi belum sampai, kita buat dummy saja.
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
+        )
+    }
+}