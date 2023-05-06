indraazimi/galeri-hewan

Menampilkan data dengan adapter

app/src/main/java/com/indraazimi/galerihewan/MainAdapter.kt
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.galerihewan
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.galerihewan.databinding.ListItemBinding
+import com.indraazimi.galerihewan.model.Hewan
+
+class MainAdapter(
+    private val data: List<Hewan>
+) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class ViewHolder(
+        private val binding: ListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(hewan: Hewan) = with(binding) {
+            namaTextView.text = hewan.nama
+            latinTextView.text = hewan.namaLatin
+        }
+    }
+}

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/MainAdapter.kt
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2021-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.mobpro1.databinding.ListItemBinding
+
+class MainAdapter(
+    private val data: List<Hewan>
+) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class ViewHolder(
+        private val binding: ListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(hewan: Hewan) = with(binding) {
+            namaTextView.text = hewan.nama
+            latinTextView.text = hewan.namaLatin
+        }
+    }
+}