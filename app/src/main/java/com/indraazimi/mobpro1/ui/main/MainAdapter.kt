indraazimi/galeri-hewan

Menampilkan gambar dari internet

app/src/main/java/com/indraazimi/galerihewan/ui/main/MainAdapter.kt
@@ -12,8 +12,11 @@ package com.indraazimi.galerihewan.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.indraazimi.galerihewan.R
 import com.indraazimi.galerihewan.databinding.ListItemBinding
 import com.indraazimi.galerihewan.model.Hewan
+import com.indraazimi.galerihewan.network.HewanApi
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
@@ -43,10 +46,15 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         private val binding: ListItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(hewan: Hewan) = with(binding) {
-            namaTextView.text = hewan.nama
-            latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(hewan.imageResId)
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_baseline_broken_image_24)
+                    .into(imageView)
+            }
         }
     }
 }

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/ui/main/MainAdapter.kt
@@ -13,9 +13,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.indraazimi.mobpro1.R
 import com.indraazimi.mobpro1.databinding.ListItemBinding
 import com.indraazimi.mobpro1.model.Hewan
+import com.indraazimi.mobpro1.network.HewanApi
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
@@ -48,7 +50,10 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         fun bind(hewan: Hewan) = with(binding) {
             namaTextView.text = hewan.nama
             latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(R.drawable.angsa)
+            Glide.with(imageView.context)
+                .load(HewanApi.getHewanUrl(hewan.imageId))
+                .error(R.drawable.ic_baseline_broken_image_24)
+                .into(imageView)
 
             root.setOnClickListener {
                 val message = root.context.getString(R.string.message, hewan.nama)