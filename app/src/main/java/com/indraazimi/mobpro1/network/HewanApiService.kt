indraazimi/galeri-hewan

Memproses data internet sebagai JSON

app/src/main/java/com/indraazimi/galerihewan/network/HewanApiService.kt
@@ -9,20 +9,27 @@
 
 package com.indraazimi.galerihewan.network
 
+import com.indraazimi.galerihewan.model.Hewan
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://dif.indraazimi.com/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface HewanApiService {
     @GET("listhewan.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/network/HewanApiService.kt
@@ -9,21 +9,28 @@
 
 package com.indraazimi.mobpro1.network
 
+import com.indraazimi.mobpro1.model.Hewan
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/" +
         "indraazimi/mobpro1/static-api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface HewanApiService {
     @GET("static-api.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {