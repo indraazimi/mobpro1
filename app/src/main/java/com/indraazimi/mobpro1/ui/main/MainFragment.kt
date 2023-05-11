indraazimi/galeri-hewan

Menambahkan loading indicator

app/src/main/java/com/indraazimi/galerihewan/ui/main/MainFragment.kt
@@ -18,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.indraazimi.galerihewan.databinding.FragmentMainBinding
+import com.indraazimi.galerihewan.network.ApiStatus
 
 class MainFragment : Fragment() {
 
@@ -46,5 +47,24 @@ class MainFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+
+        viewModel.getStatus().observe(viewLifecycleOwner, {
+            updateProgress(it)
+        })
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
+        }
     }
 }

app/src/main/java/com/indraazimi/galerihewan/ui/main/MainViewModel.kt
@@ -15,12 +15,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.indraazimi.galerihewan.model.Hewan
+import com.indraazimi.galerihewan.network.ApiStatus
 import com.indraazimi.galerihewan.network.HewanApi
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private val data = MutableLiveData<List<Hewan>>()
+    private val status = MutableLiveData<ApiStatus>()
 
     init {
         retrieveData()
@@ -28,13 +30,18 @@ class MainViewModel : ViewModel() {
 
     private fun retrieveData() {
         viewModelScope.launch {
+            status.value = ApiStatus.LOADING
             try {
                 data.value = HewanApi.service.getHewan()
+                status.value = ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
+                status.value = ApiStatus.FAILED
             }
         }
     }
 
     fun getData(): LiveData<List<Hewan>> = data
+
+    fun getStatus(): LiveData<ApiStatus> = status
 }

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/ui/main/MainFragment.kt
@@ -18,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.indraazimi.mobpro1.databinding.FragmentMainBinding
+import com.indraazimi.mobpro1.network.ApiStatus
 
 class MainFragment : Fragment() {
 
@@ -45,6 +46,25 @@ class MainFragment : Fragment() {
 
         viewModel.getData().observe(viewLifecycleOwner) {
             myAdapter.updateData(it)
+        }
+
+        viewModel.getStatus().observe(viewLifecycleOwner) {
+            updateProgress(it)
+        }
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
         }
     }
 }

app/src/main/java/com/indraazimi/mobpro1/ui/main/MainViewModel.kt
@@ -15,6 +15,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.indraazimi.mobpro1.model.Hewan
+import com.indraazimi.mobpro1.network.ApiStatus
 import com.indraazimi.mobpro1.network.HewanApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -22,6 +23,7 @@ import kotlinx.coroutines.launch
 class MainViewModel : ViewModel() {
 
     private val data = MutableLiveData<List<Hewan>>()
+    private val status = MutableLiveData<ApiStatus>()
 
     init {
         retrieveData()
@@ -29,13 +31,18 @@ class MainViewModel : ViewModel() {
 
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
+            status.postValue(ApiStatus.LOADING)
             try {
                 data.postValue(HewanApi.service.getHewan())
+                status.postValue(ApiStatus.SUCCESS)
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
+                status.postValue(ApiStatus.FAILED)
             }
         }
     }
 
     fun getData(): LiveData<List<Hewan>> = data
+
+    fun getStatus(): LiveData<ApiStatus> = status
 }
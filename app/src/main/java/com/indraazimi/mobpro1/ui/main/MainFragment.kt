68ef35c9667c5e8b8caedc7f12b19b4b6728248c
Indra Azimi <indraazimi@yahoo.com>
2021-04-22 15:12:37 +0700

    Menambahkan loading indicator

--- app/src/main/java/com/indraazimi/mobpro1/ui/main/MainFragment.kt
+++ app/src/main/java/com/indraazimi/mobpro1/ui/main/MainFragment.kt
@@ -18,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.indraazimi.mobpro1.databinding.FragmentMainBinding
+import com.indraazimi.mobpro1.network.ApiStatus
 
 class MainFragment : Fragment() {
 
@@ -45,6 +46,25 @@ class MainFragment : Fragment() {
 
         viewModel.getData().observe(viewLifecycleOwner) {
             myAdapter.updateData(it)
+        }
+
+        viewModel.getStatus().observe(viewLifecycleOwner) {
+            updateProgress(it)
+        }
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
         }
     }
 }
