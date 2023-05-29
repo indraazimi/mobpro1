indraazimi/galeri-hewan

Timer sebagai background process

app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
@@ -12,18 +12,23 @@ package com.indraazimi.galerihewan
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.galerihewan.util.MyTimer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myTimer: MyTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        myTimer = MyTimer()
         Log.i("MainActivity", "onCreate dipanggil.")
     }
 
     override fun onStart() {
         super.onStart()
+        myTimer.startTimer()
         Log.i("MainActivity", "onStart dipanggil.")
     }
 
@@ -39,6 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         Log.i("MainActivity", "onStop dipanggil.")
+        myTimer.stopTimer()
         super.onStop()
     }
 

app/src/main/java/com/indraazimi/galerihewan/util/MyTimer.kt
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2022 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.galerihewan.util
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+
+class MyTimer {
+
+    private var secondsCount = 0
+
+    private var handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable: Runnable
+
+    fun startTimer() {
+        runnable = Runnable {
+            secondsCount++
+            Log.i("Timer", "Detik ke: $secondsCount")
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    fun stopTimer() {
+        handler.removeCallbacks(runnable)
+    }
+}

indraazimi/mobpro1

app/src/main/java/com/indraazimi/mobpro1/MainActivity.kt
@@ -12,18 +12,23 @@ package com.indraazimi.mobpro1
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.mobpro1.util.MyTimer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myTimer: MyTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        myTimer = MyTimer()
         Log.i("MainActivity", "onCreate dipanggil.")
     }
 
     override fun onStart() {
         super.onStart()
+        myTimer.startTimer()
         Log.i("MainActivity", "onStart dipanggil.")
     }
 
@@ -39,6 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         Log.i("MainActivity", "onStop dipanggil.")
+        myTimer.stopTimer()
         super.onStop()
     }
 

app/src/main/java/com/indraazimi/mobpro1/util/MyTimer.kt
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2021-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.util
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+
+class MyTimer {
+
+    private var secondsCount = 0
+
+    private var handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable: Runnable
+
+    fun startTimer() {
+        runnable = Runnable {
+            secondsCount++
+            Log.i("Timer", "Detik ke: $secondsCount")
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    fun stopTimer() {
+        handler.removeCallbacks(runnable)
+    }
+}