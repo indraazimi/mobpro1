48aa5402d38d1e6c43ed66d97e317817a2456660
Indra Azimi <indraazimi@yahoo.com>
2023-06-04 22:42:58 +0700

    Membuat notifikasi updater

--- app/src/main/java/com/indraazimi/mobpro1/network/UpdateWorker.kt
+++ app/src/main/java/com/indraazimi/mobpro1/network/UpdateWorker.kt
@@ -10,12 +10,19 @@
 package com.indraazimi.mobpro1.network
 
 import android.Manifest
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.indraazimi.mobpro1.MainActivity
+import com.indraazimi.mobpro1.R
 
 class UpdateWorker(
     context: Context,
@@ -24,9 +31,18 @@ class UpdateWorker(
 
     companion object {
         const val WORK_NAME = "updater"
+        private const val NOTIFICATION_ID = 44
     }
 
     override suspend fun doWork(): Result {
+        // Update data di background dilakukan dalam 3 tahapan:
+        // 1. Request data yang baru ke server
+        // 2. Simpan di database sebagai cache
+        // 3. Beri tahu pengguna ada data baru
+
+        // Langkah 1 dan 2 sudah diajarkan di modul sebelumnya.
+        // Jadi di sini kita langsung loncat ke langkah 3 saja.
+
         if (ActivityCompat.checkSelfPermission(
                 applicationContext,
                 Manifest.permission.POST_NOTIFICATIONS
@@ -36,8 +52,27 @@ class UpdateWorker(
             return Result.failure()
         }
 
-        Log.d("Worker", "Menjalankan proses background..")
+        val builder = NotificationCompat.Builder(applicationContext, MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(R.string.notif_title))
+            .setContentText(applicationContext.getString(R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(getPendingIntent(applicationContext))
+            .setAutoCancel(true)
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(NOTIFICATION_ID, builder.build())
 
         return Result.success()
+    }
+
+    private fun getPendingIntent(context: Context): PendingIntent {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        var flags = PendingIntent.FLAG_UPDATE_CURRENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            flags = flags or PendingIntent.FLAG_IMMUTABLE
+        }
+        return PendingIntent.getActivity(context, 0, intent, flags)
     }
 }
