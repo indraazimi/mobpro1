cf2dba706560c48137dd40cb260f9246e636cfa1
Indra Azimi <indraazimi@yahoo.com>
2023-06-04 22:16:50 +0700

    Meminta izin notifikasi

--- app/src/main/java/com/indraazimi/mobpro1/network/UpdateWorker.kt
+++ app/src/main/java/com/indraazimi/mobpro1/network/UpdateWorker.kt
@@ -9,8 +9,11 @@
 
 package com.indraazimi.mobpro1.network
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 
@@ -24,6 +27,15 @@ class UpdateWorker(
     }
 
     override suspend fun doWork(): Result {
+        if (ActivityCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e("Worker", "Tidak diberikan izin notifikasi")
+            return Result.failure()
+        }
+
         Log.d("Worker", "Menjalankan proses background..")
 
         return Result.success()
--- app/src/main/java/com/indraazimi/mobpro1/ui/main/MainFragment.kt
+++ app/src/main/java/com/indraazimi/mobpro1/ui/main/MainFragment.kt
@@ -9,14 +9,20 @@
 
 package com.indraazimi.mobpro1.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.mobpro1.MainActivity
 import com.indraazimi.mobpro1.databinding.FragmentMainBinding
 import com.indraazimi.mobpro1.network.ApiStatus
 
@@ -62,11 +68,32 @@ class MainFragment : Fragment() {
             }
             ApiStatus.SUCCESS -> {
                 binding.progressBar.visibility = View.GONE
+
+                // izin notifikasi diminta ketika diperlukan saja (Android 13)
+                // minta izinnya tidak langsung, tapi setelah berhasil loading
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestNotificationPermission()
+                }
             }
             ApiStatus.FAILED -> {
                 binding.progressBar.visibility = View.GONE
                 binding.networkError.visibility = View.VISIBLE
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
         }
     }
 }
